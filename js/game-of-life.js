4383190ada2500dfa0ae5755d3c18656e620dea5
Exidex <16986685+Exidex@users.noreply.github.com>
2024-08-04 18:07:39 +0000

    chore: deploy 478c1f187601b8f572de06fa0261c7072fd2d32c

--- js/game-of-life.js
+++ js/game-of-life.js
@@ -3,6 +3,5 @@ import 'react';
 import '@project-gauntlet/api/components';
 import './vendor.js';
 export { G as default } from './shared.js';
-import '@project-gauntlet/api/helpers';
 import '@project-gauntlet/api/hooks';
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZ2FtZS1vZi1saWZlLmpzIiwic291cmNlcyI6W10sInNvdXJjZXNDb250ZW50IjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7OyJ9
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZ2FtZS1vZi1saWZlLmpzIiwic291cmNlcyI6W10sInNvdXJjZXNDb250ZW50IjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7In0=
--- js/starwars-species.js
+++ js/starwars-species.js
@@ -4,5 +4,4 @@ import '@project-gauntlet/api/hooks';
 export { a as default } from './shared.js';
 import 'react';
 import './vendor.js';
-import '@project-gauntlet/api/helpers';
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic3RhcndhcnMtc3BlY2llcy5qcyIsInNvdXJjZXMiOltdLCJzb3VyY2VzQ29udGVudCI6W10sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7OzsifQ==
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic3RhcndhcnMtc3BlY2llcy5qcyIsInNvdXJjZXMiOltdLCJzb3VyY2VzQ29udGVudCI6W10sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7OyJ9
