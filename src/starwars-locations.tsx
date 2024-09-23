9d9c5a6a324f399623027db9c8f537e463784577
Exidex <16986685+Exidex@users.noreply.github.com>
2024-09-23 17:54:34 +0200

    Fix missing images

--- src/starwars-locations.tsx
+++ src/starwars-locations.tsx
@@ -5,31 +5,49 @@ export default function StarWarsLocations(): ReactElement {
     return (
         <Grid>
             <Grid.Item title="Naboo">
-                <Grid.Item.Content.Image source={{ asset: "naboo.jpg" }}/>
+                <Grid.Item.Content>
+                    <Grid.Item.Content.Image source={{ asset: "naboo.jpg" }}/>
+                </Grid.Item.Content>
             </Grid.Item>
             <Grid.Item title="Ryloth">
-                <Grid.Item.Content.Image source={{ asset: "dagobah.jpg" }}/>
+                <Grid.Item.Content>
+                    <Grid.Item.Content.Image source={{ asset: "dagobah.jpg" }}/>
+                </Grid.Item.Content>
             </Grid.Item>
             <Grid.Item title="Tatooine">
-                <Grid.Item.Content.Image source={{ asset: "ryloth.jpg" }}/>
+                <Grid.Item.Content>
+                    <Grid.Item.Content.Image source={{ asset: "ryloth.jpg" }}/>
+                </Grid.Item.Content>
             </Grid.Item>
             <Grid.Item title="Dagobah">
-                <Grid.Item.Content.Image source={{ asset: "tatooine.jpg" }}/>
+                <Grid.Item.Content>
+                    <Grid.Item.Content.Image source={{ asset: "tatooine.jpg" }}/>
+                </Grid.Item.Content>
             </Grid.Item>
             <Grid.Item title="Coruscant">
-                <Grid.Item.Content.Image source={{ asset: "deathstar.jpg" }}/>
+                <Grid.Item.Content>
+                    <Grid.Item.Content.Image source={{ asset: "deathstar.jpg" }}/>
+                </Grid.Item.Content>
             </Grid.Item>
             <Grid.Item title="Endor">
-                <Grid.Item.Content.Image source={{ asset: "endor.jpg" }}/>
+                <Grid.Item.Content>
+                    <Grid.Item.Content.Image source={{ asset: "endor.jpg" }}/>
+                </Grid.Item.Content>
             </Grid.Item>
             <Grid.Item title="Death Star">
-                <Grid.Item.Content.Image source={{ asset: "coruscant.jpg" }}/>
+                <Grid.Item.Content>
+                    <Grid.Item.Content.Image source={{ asset: "coruscant.jpg" }}/>
+                </Grid.Item.Content>
             </Grid.Item>
             <Grid.Item title="Dathomir">
-                <Grid.Item.Content.Image source={{ asset: "dathomir.jpg" }}/>
+                <Grid.Item.Content>
+                    <Grid.Item.Content.Image source={{ asset: "dathomir.jpg" }}/>
+                </Grid.Item.Content>
             </Grid.Item>
             <Grid.Item title="Dantooine">
-                <Grid.Item.Content.Image source={{ asset: "dantooine.jpg" }}/>
+                <Grid.Item.Content>
+                    <Grid.Item.Content.Image source={{ asset: "dantooine.jpg" }}/>
+                </Grid.Item.Content>
             </Grid.Item>
         </Grid>
     )
